6991299275a0b195a6b06f5c37124d572f2ab002
jberroa <jberroa@agile-im.de>
2019-06-12 13:13:27 +0200

    Parser extension for max score

--- scripts/play.py
+++ scripts/play.py
@@ -12,19 +12,25 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 def parse():
     parser = argparse.ArgumentParser(description='Optionally pass in Connect4 parameters via commandline.')
 
+    parser.add_argument('--max_score', type=int, default=0, help="Max score to win the entire game.")
     parser.add_argument('-x', type=int, default=0, help='X dimension of the board')
     parser.add_argument('-y', type=int, default=0, help='Y dimension of the board')
     args = parser.parse_args()
     LOG.debug("Args are: {}".format(args))
-    return args.x, args.y
+    return args.x, args.y, args.max_score
 
 
 if __name__ == '__main__':
-    x, y = parse()
+    x, y, max_score = parse()
+
+    # Change field size if desired
     if x == 0 and y == 0:
         kwargs = {}
     else:
         kwargs = {"x": x, "y": y}
+    # Change max score if desired
+    if max_score == 0:
+        max_score = 3
 
-    env = environments.HumanEnvironment(max_score=2, **kwargs)
+    env = environments.HumanEnvironment(max_score=max_score, **kwargs)
     env.play_match()
